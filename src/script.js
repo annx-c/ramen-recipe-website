ca64ff14cf56815c91f567fe8a5af1610417eb77
Ana Cañete <ana.canete13@gmail.com>
2025-03-06 18:49:23 -0300

    Code is added to script.js, which allows displaying one recipe at a time

--- src/script.js
+++ src/script.js
@@ -37,3 +37,32 @@ function typewriterEffect(selector, textArray) {
 
 // Llama a la función para inicializar los efectos de máquina de escribir - Calls the function to initialize typewriter effects
 initializeTypewriterEffects();
+function recipeRamen(event) {
+  event.preventDefault();
+  console.log(event.target.id);
+
+  // Ocultar cualquier receta visible - Hide any visible recipe
+  let visibleRecipes = document.querySelectorAll(".recipe-items:not(.display-recipe)"); // Selecciona todas las recetas que no tienen la clase display-recipe - Select all recipes that do not have the class display-recipe
+  visibleRecipes.forEach((recipe) => { // Oculta todas las recetas que no tienen la clase display-recipe - Hide all recipes that do not have the class display-recipe
+    recipe.classList.add("display-recipe");
+    recipe.classList.remove("active");
+  });
+
+  // Mostrar la receta correspondiente al botón presionado - Show the recipe corresponding to the pressed button
+  let recipeId = `#recipe-${event.target.id}`; //Se agrega el id del botón al id de la receta con el inicio de rec "recipe-" para determinar la receta a mostrar - The id of the button is added to the id of the recipe with the beginning of rec "recipe-" to determine the recipe to show
+  let selectedRecipe = document.querySelector(recipeId);
+  if (selectedRecipe) { // Si la receta existe, se muestra - If the recipe exists, it is shown
+    selectedRecipe.classList.remove("display-recipe");
+
+    void selectedRecipe.offsetWidth; // Reinicia la animación - Restart the animation
+    selectedRecipe.classList.add("active");
+  }
+}
+
+// Asegúrate de que el selector apunta a todos los botones correctos
+let buttonRamenElements = document.querySelectorAll(".recipe-button");
+
+buttonRamenElements.forEach((button) => {
+  button.addEventListener("click", recipeRamen);
+});
+
